41e6164ac36d06b5359329752206449832a0fc35
shirosuke <tuketarou@gmail.com>
2018-12-16 12:27:49 +0900

    1.22 解きかけ

--- SICP/shirokuma/chapter1/exersice.rkt
+++ SICP/shirokuma/chapter1/exersice.rkt
@@ -204,5 +204,30 @@ TODO
 
 TODO
 
+; 1.20
+
+; 1.21
+
+#lang sicp
+
+(define (smallest-divisor n) (find-divisor n 2)) (define (find-divisor n test-divisor) 
+(cond ((> (square test-divisor) n) n) ((divides? test-divisor n) test-divisor) (else (find-divisor n (+ test-divisor 1))))) 
+(define (divides? a b) (= (remainder b a) 0)) 
+(define (square x) (* x x))
+
+
+(smallest-divisor 199)
+  -> 199
+(smallest-divisor 1999)
+  -> 1999
+(smallest-divisor 19999)
+  -> 7
+
+
+; 1.22
+
+
+
+
 
 
