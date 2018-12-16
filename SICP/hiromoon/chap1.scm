a16600be5e4020f701b814d2a9aba41f35f6257b
Hiromu Yoshiwara <hiromoon0428@gmail.com>
2018-12-16 13:10:14 +0900

    1-23

--- SICP/hiromoon/chap1.scm
+++ SICP/hiromoon/chap1.scm
@@ -327,3 +327,31 @@
 ;1009,   1013,   1019
 ;10007,  10009,  10037
 ;100003, 100019, 100043
+
+;practice1-23
+(define (timed-prime-test n) (newline)
+(display n)
+(start-prime-test n (runtime)))
+(define (start-prime-test n start-time) (if (prime? n)
+(report-prime (- (runtime) start-time)))) (define (report-prime elapsed-time)
+(display " *** ") (display elapsed-time))
+
+(define (prime? n)
+(= n (smallest-divisor n)))
+
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+(define (divides? a b) (= (remainder b a) 0))
+  
+(define (square n) (* n n))
+
+(define (search-for-primes n max)
+  (cond ((< max n) (display "finish"))
+        (else (timed-prime-test n) (search-for-primes (+ n 2) max))
+      ))
+(define (next n)
+  (cond ((= n 2) 3)
+        (else (+ n 2))))
