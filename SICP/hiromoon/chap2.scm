47206225c548970e6cacf0f7b97edd4e1e9f5998
hiromoon <hiromoon0428@gmail.com>
2019-01-08 00:46:16 +0900

    2-21まで回答追加

--- SICP/hiromoon/chap2.scm
+++ SICP/hiromoon/chap2.scm
@@ -153,3 +153,109 @@
 
 (((plus one two) succ) 0) ;3
 (((plus two two) succ) 0) ;4
+
+;practice2-7
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y) 
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval
+    x
+    (make-interval (/ 1.0 (upper-bound y))
+                   (/ 1.0 (lower-bound y)))))
+
+(define (make-interval a b) (cons a b))
+(define (upper-bound interval)
+  (car interval))
+(define (lower-bound interval)
+  (cdr interval))
+
+;practice2-8
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (lower-bound y))
+                 (- (upper-bound x) (upper-bound y))))
+
+;practice2-9
+(define (interval-width interval) 
+  (/
+    (- (upper-bound interval) (lower-bound interval))
+    2))
+;TODO: 発展問題なので2-16までは一旦スキップ
+
+;practice2-17
+(define (last-pair l)
+  (if (null? (cdr l))
+    (list (car l))
+    (last-pair (cdr l))))
+
+(last-pair (list 23 72 149 34))
+
+;practice2-18
+(define (reverse l)
+  (if (null? (cdr l))
+    (list (car l))
+    (append
+      (reverse (cdr l))
+      (list (car l)))))
+
+(reverse (list 1 4 9 16 25))
+
+;practice2-19
+;リストに対して全探索しているため
+;リストの順番によって答えに影響を与えない
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount
+                 (except-first-denomination coin-values))
+             (cc (- amount
+                    (first-denomination coin-values)) 
+                 coin-values)))))
+
+(define first-denomination car)
+(define except-first-denomination cdr)
+(define no-more? null?)
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(cc 100 us-coins)
+(cc 100 uk-coins)
+
+;practice2-20
+(define (same-parity n . ns)
+  (define (iter is_even ns acc)
+    (if (null? ns)
+      acc
+      (if (= is_even (remainder (car ns))) 
+        (iter is_even (cdr ns) (append acc (list (car ns))))
+        (iter is_even (cdr ns) acc)))
+  (iter (remainder n) ns (list n)))
+
+(same-parity 1 2 3 4 5 6 7)
+(same-parity 2 3 4 5 6 7)
+
+;practice2-21
+(define (square-list items)
+  (if (null? items)
+    nil
+    (cons
+      (square (car items))
+      (square-list (cdr items)))))
+
+(square-list (list 1 2 3 4))
+
+(define (square-list items)
+  (map square items))
+
+(square-list (list 1 2 3 4))
