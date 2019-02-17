1984b56a7460ff9ca06dbe6e7056e59e3f2cc6e9
Hiromu Yoshiwara <hiromoon0428@gmail.com>
2019-02-17 14:37:19 +0900

    2-51まで回答追加

--- SICP/hiromoon/chap2.scm
+++ SICP/hiromoon/chap2.scm
@@ -742,3 +742,47 @@ ubsets s) (if (null? s)
         (make-segment point4 point1)))))
 ;d. waveペインタ
 ;めんどいからすきっぷ
+
+;practice2-50
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+;practice2-51
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-top 
+            (transform-painter
+              painter2
+              split-point
+              (make-vect 1.0 0.5)
+              (make-vect 1.0 1.0))) 
+          (paint-bottom
+            (transform-painter
+              painter1
+              (make-vect 0.0 0.0)
+              (make-vect 1.0 0.0)
+              split-point)))
+      (lambda (frame)
+        (paint-top frame)
+        (paint-bottom frame)))))
+
+(define (below painter1 painter2) 
+  (rotate90
+    (beside
+      (rotate270 painter1)
+      (rotate270 painter2))))
