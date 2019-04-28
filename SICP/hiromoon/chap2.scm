28f870dc5aea25690ebbd49cba0f9bddf73937fe
hiromoon <hiromoon0428@gmail.com>
2019-04-28 14:56:50 +0900

    2-84まで

--- SICP/hiromoon/chap2.scm
+++ SICP/hiromoon/chap2.scm
@@ -1451,4 +1451,69 @@ ubsets s) (if (null? s)
     (let ((proc (get op type-tags)))
       (if (not (null? proc))
         (apply proc (map contents args))
-        (apply applygeneric (cons op (iter type-tags args)))))))
+        (apply apply-generic (cons op (iter type-tags args)))))))
+
+;practice2-83
+(define (raise x) (apply-generic 'raise x))
+
+(put 'raise 'integer
+     (lambda (x) (make-rational x 1)))
+(put 'raise 'rational
+     (lambda (x) (make-real (/ (numer x) (denom x)))))
+(put 'raise 'real
+     (lambda (x) (make-from-real-imag x 0)))
+;apply-genericがまだよくわかってない気がする
+
+;practice2-84
+(define (level type)
+  (cond ((eq? type 'integer) 0)
+        ((eq? type 'rational) 1)
+        ((eq? type 'real) 2)
+        ((eq? type 'complex) 3)
+        (else (error "Invaid type: LEVEL" type))))
+
+(define (highest-type args)
+  (if (null? (cdr args))
+    (type-tag (car args))
+    (let ((t1 (type-tag (car args)))
+          (t2 (highest-type (cdr args))))
+      (let ((l1 (level t1)) (l2 (level t2)))
+        (if (> l1 l2) t1 t2)))))
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (define (no-method)
+      (error "No method for these types" (list op type-tags)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contents args))
+        (if (not (null? (cdr args)))
+          (let ((raised-args (raise-to-common args)))
+            (if raised-args
+              (let ((proc (get op (map type-tag raised-args))))
+                (if proc
+                  (apply proc (map contents raised-args))
+                  (no-method)))
+              (no-method)))
+          (no-method))))))
+
+(define (raise-to-common args)
+  (let ((raised-args
+          (map (lambda (x) (raised-to-type (highest-type args) x))
+               args)))
+    (if (all-true? raised-args)
+      raised-args?
+      false)))
+
+(define (all-true? lst)
+  (cond ((null? lst) true)
+        ((car list) (all-true? (cdr lst)))
+        (else false)))
+
+(define (raise-to-type type item)
+  (let ((item-type (type-tag item)))
+    (if (eq? item-type type)
+      item
+      (let ((raise-fn (get 'raise item-type)))
+        (if raise-fn
+          (raise-to-type type (raise-fn item))
+          false)))))
