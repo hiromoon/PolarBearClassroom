efb22c45a46444b2ce7fcda68deb7202c88fe60b
hiromoon <hiromoon0428@gmail.com>
2019-12-08 17:38:00 +0900

    テストの設定ファイルを追加

---
+++ CinemaTicket/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  roots: [
+    '<rootDir>',
+  ],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+      diagnostics: false, 
+    },
+  },
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testRegex: '(__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+  ],
+};
