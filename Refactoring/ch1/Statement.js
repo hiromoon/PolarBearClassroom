90b6fc2654ba772c05ee1791f8fd13a4fb07e16d
shirosuke <tuketarou@gmail.com>
2020-05-23 23:18:58 +0900

    refactor: 関数宣言の変更準備

--- Refactoring/ch1/Statement.js
+++ Refactoring/ch1/Statement.js
@@ -21,32 +21,51 @@ function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits \n`;
     return result;
 
+    function amountFor(aPerformance, play) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+        return result;
+    }
+
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
 }
 
-function amountFor(aPerformance, play) {
-    let result = 0;
-    switch (play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${play.type}`);
-    }
-    return result;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
