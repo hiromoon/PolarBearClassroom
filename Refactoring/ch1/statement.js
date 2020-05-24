06a4816abac7cbb4e6e43a18663170897e5caa2b
shirosuke <tuketarou@gmail.com>
2020-05-24 12:43:27 +0900

    refactor: 変数名の変更

--- Refactoring/ch1/statement.js
+++ Refactoring/ch1/statement.js
@@ -39,13 +39,13 @@ function statement(invoice, plays) {
         return result;
     }
 
-    function volumeCreditsFor(perf) {
-        let volumeCredits = 0;
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
         // ボリューム特典のポイントを加算
-        volumeCredits += Math.max(perf.audience - 30, 0);
+        result += Math.max(aPerformance.audience - 30, 0);
         // 喜劇のときは10人につき、さらにポイントを加算
-        if ("comedy" === playFor(perf).type) { volumeCredits += Math.floor(perf.audience / 5); }
-        return volumeCredits;
+        if ("comedy" === playFor(aPerformance).type) { result += Math.floor(aPerformance.audience / 5); }
+        return result;
     }
 
     function playFor(aPerformance) {
