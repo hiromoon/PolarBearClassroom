8744665eea14148ef570168e8a2dbf95bc4ecc42
shirosuke <tuketarou@gmail.com>
2020-05-31 00:50:18 +0900

    refactor: 計算部分をファイルとして独立

--- Refactoring/shirosuke/ch1/statement.js
+++ Refactoring/shirosuke/ch1/statement.js
@@ -1,66 +1,8 @@
+const createStatementData = require ('./createStatementData');
+//import createStatementData from './createStatementData.js';
+
 function statement(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);
-
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance);
-        // TODO オブジェクト渡してない? copyした方が良いのでは?
-        result.play =  playFor(result); 
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                // FIXME ここ通ったらバグる
-                throw new Error(`unknown type: ${play.type}`);
-        }
-        return result;
-    }
-
-    function totalAmount(data) {
-        return data.performances
-            .reduce((total, p) => total + p.amount, 0);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        // ボリューム特典のポイントを加算
-        result += Math.max(aPerformance.audience - 30, 0);
-        // 喜劇のときは10人につき、さらにポイントを加算
-        if ("comedy" === aPerformance.play.type) { result += Math.floor(aPerformance.audience / 5); }
-        return result;
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances
-            .reduce((total, p) => total + p.volumeCredits, 0);
-    }
-
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
 function renderPlainText(data) {
