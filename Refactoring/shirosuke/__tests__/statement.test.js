2ea6dc2ca9a61c35cafdba7b8b7ae6dafc94735a
shirosuke <tuketarou@gmail.com>
2020-05-31 01:40:42 +0900

    feat: HTML版追加

--- Refactoring/shirosuke/__tests__/statement.test.js
+++ Refactoring/shirosuke/__tests__/statement.test.js
@@ -26,7 +26,7 @@ const invoices = [
     }
 ]
 
-const expected = `Statement for BigCo
+const expectedText = `Statement for BigCo
  Hamlet: $650.00 (55 seats)
  As You Like It: $580.00 (35 seats)
  Othello: $500.00 (40 seats)
@@ -34,6 +34,21 @@ Amount owed is $1,730.00
 You earned 47 credits 
 `;
 
-test('regression test', () => {
-    expect(statement(invoices[0], plays)).toBe(expected);
+test('statement test', () => {
+    expect(statement.statement(invoices[0], plays)).toBe(expectedText);
 });
+
+const expectedHtml =  `<h1>Statment for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount Owed is <em>$1,730.00</em></p>
+<p> You earned <em>47</em> credits</p>
+`;
+
+test('htmlStatemnt test', () => {
+    expect(statement.htmlStatement(invoices[0], plays)).toBe(expectedHtml);
+});
+
--- Refactoring/shirosuke/ch1/statement.js
+++ Refactoring/shirosuke/ch1/statement.js
@@ -1,8 +1,11 @@
-const createStatementData = require ('./createStatementData');
-//import createStatementData from './createStatementData.js';
+const createStatementData = require('./createStatementData');
 
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
+}
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
 }
 
 function renderPlainText(data) {
@@ -15,10 +18,28 @@ function renderPlainText(data) {
 
     result += `Amount owed is ${usd(data.totalAmount)}\n`;
     result += `You earned ${data.totalVolumeCredits} credits \n`;
-    return result; 
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 2 }).format(aNumber / 100);
-    }    
+    return result;
 }
-module.exports = statement;
+
+function renderHtml(data) {
+    let result = `<h1>Statment for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (const perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount Owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p> You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 2 }).format(aNumber / 100);
+}
+
+module.exports = {
+    statement,
+    htmlStatement
+}
