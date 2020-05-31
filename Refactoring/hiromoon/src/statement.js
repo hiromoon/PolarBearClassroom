84a2b57f6bf5f7d3a80a6f99ccc81a0e198ab38f
hiromoon <hiromoon0428@gmail.com>
2020-05-31 16:50:54 +0900

    単位の変更を関数内へ取り込み

--- Refactoring/hiromoon/src/statement.js
+++ Refactoring/hiromoon/src/statement.js
@@ -38,7 +38,7 @@ module.exports = function statement(invoice, plays) {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 
   let volumeCredits = 0;
@@ -48,10 +48,10 @@ module.exports = function statement(invoice, plays) {
   for (const perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
-    result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 };
