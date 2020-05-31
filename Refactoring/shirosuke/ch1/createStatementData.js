6048b91ffd9c91d696165bfe98e9055daf4daed4
shirosuke <tuketarou@gmail.com>
2020-05-31 16:03:02 +0900

    refactor: 料金計算部分をボリモーフィックに

--- Refactoring/shirosuke/ch1/createStatementData.js
+++ Refactoring/shirosuke/ch1/createStatementData.js
@@ -10,41 +10,23 @@ function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         const calculator = new createPerformanceCalculator(aPerformance, playFor(result));
         // TODO オブジェクト渡してない? copyした方が良いのでは?
-        result.play =  calculator.play;
-        result.amount = amountFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
     function createPerformanceCalculator(aPerformance, aPlay) {
-        return new PerformanceCalculator(aPerformance, aPlay);
+        switch (aPlay.type) {
+            case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+            case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+            default:
+                throw new Error(`未知の演劇の種類： ${aPlay.type}`);
+        }
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                // FIXME ここ通ったらバグる
-                throw new Error(`unknown type: ${play.type}`);
-        }
-        return result;
     }
 
     function totalAmount(data) {
@@ -73,25 +55,27 @@ class PerformanceCalculator {
         this.play = aPlay;
     }
     amountFor() {
-        let result = 0;
-        switch (this.performance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                // FIXME ここ通ったらバグる
-                throw new Error(`unknown type: ${play.type}`);
+        throw Error("サブクラスの責務");
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
         }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
         return result;
     }
 }
