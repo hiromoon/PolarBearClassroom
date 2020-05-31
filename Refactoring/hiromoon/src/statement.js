20beab9205db92c132df86fd28682f2378a88900
hiromoon <hiromoon0428@gmail.com>
2020-05-31 16:48:03 +0900

    format を usd へ関数宣言を変更

--- Refactoring/hiromoon/src/statement.js
+++ Refactoring/hiromoon/src/statement.js
@@ -33,7 +33,7 @@ module.exports = function statement(invoice, plays) {
     return volumeCredits;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
@@ -48,10 +48,10 @@ module.exports = function statement(invoice, plays) {
   for (const perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 };
