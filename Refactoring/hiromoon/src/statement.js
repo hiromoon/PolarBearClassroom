0defe4bdba420b3f028bbd3bbacb08daab2d38a1
hiromoon <hiromoon0428@gmail.com>
2020-05-31 16:31:38 +0900

    thisAmountをインライン化

--- Refactoring/hiromoon/src/statement.js
+++ Refactoring/hiromoon/src/statement.js
@@ -38,12 +38,10 @@ module.exports = function statement(invoice, plays) {
   });
 
   for (const perf of invoice.performances) {
-    const thisAmount = amountFor(perf);
-
     volumeCredits += Math.max(perf.audience - 30, 0);
     if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
-    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
