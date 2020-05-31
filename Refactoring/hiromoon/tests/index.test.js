b5403f0a33121398237288f27894753297e3ddff
hiromoon <hiromoon0428@gmail.com>
2020-05-31 15:51:53 +0900

    test準備した

---
+++ Refactoring/hiromoon/tests/index.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const fs = require('fs');
+
+const statement = require('../src/statement');
+
+describe('statement.js', () => {
+  let invoices;
+  let plays;
+
+  beforeEach(() => {
+    invoices = JSON.parse(fs.readFileSync('./data/invoices.json').toString());
+    plays = JSON.parse(fs.readFileSync('./data/plays.json').toString());
+  });
+
+  describe('.statement', () => {
+    it('should be BigCo output', () => {
+      const expected = `Statement for BigCo
+Hamlet: $650.00 (55 seats)
+As You Like It: $580.00 (35 seats)
+Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`;
+      expect(statement(invoices[0], plays)).to.equal(expected);
+    });
+  });
+});
