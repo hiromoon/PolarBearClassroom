4c0d2639085d0352e9f0d006dd444d168306ac4a
hiromoon <hiromoon0428@gmail.com>
2020-06-14 15:48:37 +0900

    Calculatorの関数宣言の変更

--- Refactoring/hiromoon/src/createStatementData.js
+++ Refactoring/hiromoon/src/createStatementData.js
@@ -1,6 +1,7 @@
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
+    this.play = aPlay;
   }
 }
 
@@ -48,9 +49,9 @@ module.exports = function createStatementData(invoice, plays) {
   }
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance);
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = { ...aPerformance };
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
