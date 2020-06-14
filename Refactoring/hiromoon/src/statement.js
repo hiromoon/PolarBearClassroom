1b60a96f2091ab2a8d24c66016ed4e5a1b0d0c03
hiromoon <hiromoon0428@gmail.com>
2020-06-14 15:39:34 +0900

    HTML版を追加

--- Refactoring/hiromoon/src/statement.js
+++ Refactoring/hiromoon/src/statement.js
@@ -23,3 +23,20 @@ function renderPlainText(data) {
 module.exports = function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 };
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += '<table>\n';
+  result += '<tr.<th.play</th><th>seats</th><th>cost</th></tr>';
+  for (const perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += '</table>\n';
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
